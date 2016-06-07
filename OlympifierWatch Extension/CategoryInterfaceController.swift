928f7656481268dbbd6c67cd0c878110e1597dfe
Madhur Mohta <madhur.mohta@pearson.com>
2016-06-08 01:35:00 +0530

    Watch initial commit

---
+++ OlympifierWatch Extension/CategoryInterfaceController.swift
@@ -0,0 +1,44 @@
+//
+//  CategoryInterfaceController.swift
+//  Olympifier
+//
+//  Created by Madhur Mohta on 08/06/2016.
+//  Copyright © 2016 Pearson_3. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+@available(iOS 8.2, *)
+class CategoryInterfaceController: WKInterfaceController {
+
+   
+    @IBOutlet var tableView: WKInterfaceTable!
+    
+    var categoryModel : RioCategoryModel?{
+        didSet{
+            
+            if let model = categoryModel{
+                
+                
+            }
+        }
+    }
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ OlympifierWatch Extension/SplashInterfaceController.swift
@@ -0,0 +1,32 @@
+//
+//  SplashInterfaceController.swift
+//  Olympifier
+//
+//  Created by Madhur Mohta on 08/06/2016.
+//  Copyright © 2016 Pearson_3. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+@available(iOS 8.2, *)
+class SplashInterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
